48de64be564c251ea0529e9d56e692592b8ccbb7
N1ghtw1re <n1ghtw1re@proton.me>
2025-05-11 08:37:30 +0700

    Update news.ts

--- src/data/news.ts
+++ src/data/news.ts
@@ -8,29 +8,29 @@ export interface NewsItem {
 export const newsItems: NewsItem[] = [
   {
     id: 1,
+    date: '2025-05-11 08:36',
+    content: 'Hackers Tarot Launch is imminent.',
+  },
+  {
+    id: 2,
     date: '2024-04-16 21:45',
     content: 'Added system news ticker to home page.',
   },
   {
-    id: 2,
+    id: 3,
     date: '2024-04-16 21:45',
     content: 'New Article: Is 4Chan Doomed?',
     link: 'https://n1ghtw1re.neocities.org/articles/04-2025/4chan_hack',
   },
   {
-    id: 3,
+    id: 4,
     date: '2024-04-13 21:45',
     content: 'New Book: Into Ideaspace is available',
     link: 'https://n1ghtw1re.neocities.org/books/into_ideaspace/into_ideaspace',
   },
   {
-    id: 4,
+    id: 5,
     date: '2025-04-12 18:30',
     content: 'Full N1ghtw1re Collective web redesign is complete.',
-  },
-  {
-    id: 5,
-    date: '2024-12-18 23:15',
-    content: 'N1ghtw1re is online. Prepare for first transmission.',
   },
 ];
